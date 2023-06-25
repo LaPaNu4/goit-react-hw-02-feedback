d8e0bf3e4f67333c059781ea622a31b7639bdad4
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-06-25 20:35:05 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <h2>Please leave feedback</h2>
+        <form action=""></form>
+        <h2>Statistics</h2>
+        <p>Good:{this.state.good}</p>
+        <p>Neutral:{this.state.neutral}</p>
+        <p>Bad:{this.state.bad}</p>
+      </div>
+    );
+
+  }
+}
